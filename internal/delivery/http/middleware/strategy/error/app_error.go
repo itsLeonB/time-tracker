ddc9ff1cf0fd33e211ddfbec0c24ec97610c2089
itsLeonB <ellionblessan@gmail.com>
2024-12-16 22:04:14 +0700

    fix: handle unexpected errors and refactor error response builder

---
+++ internal/delivery/http/middleware/strategy/error/app_error.go
@@ -0,0 +1,26 @@
+package strategy
+
+import (
+	"log"
+
+	"github.com/itsLeonB/time-tracker/internal/apperror"
+	"github.com/itsLeonB/time-tracker/internal/model"
+	"github.com/rotisserie/eris"
+)
+
+type appErrorStrategy struct{}
+
+func (es *appErrorStrategy) HandleError(err error) *model.ErrorResponse {
+	appError := err.(*apperror.AppError)
+
+	if appError.HttpStatusCode == 500 {
+		log.Println(eris.ToString(appError.Err, true))
+	}
+
+	return &model.ErrorResponse{
+		Code:    appError.HttpStatusCode,
+		Type:    appError.Type,
+		Message: appError.Message,
+		Details: appError.Err.Error(),
+	}
+}
