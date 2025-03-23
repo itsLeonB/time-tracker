636e8775cc1f06a06fdacf92ee87ca87468f9a5b
itsLeonB <ellionblessan@gmail.com>
2025-03-23 12:24:51 +0700

    feat(auth): bump JWT token duration

--- internal/config/auth_config.go
+++ internal/config/auth_config.go
@@ -17,7 +17,7 @@ type Auth struct {
 func loadAuthConfig() *Auth {
 	return &Auth{
 		SecretKey:      os.Getenv("SECRET_KEY"),
-		TokenDuration:  5 * time.Hour,
+		TokenDuration:  9 * time.Hour,
 		CookieDuration: 24 * time.Hour,
 		Issuer:         os.Getenv("APP_NAME"),
 		URL:            fmt.Sprintf("%s:%s", os.Getenv("APP_HOST"), os.Getenv("APP_PORT")),
